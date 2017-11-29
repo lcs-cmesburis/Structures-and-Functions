b0458120412efff24031ecfbd952d00eac2d4ebe
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-11-29 11:40:24 -0500

    amount done after video 5

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -32,4 +32,45 @@ import Foundation
 
 */
 
-// Begin here... 
+// Our given line is: y = -1/2x = 9.5
+// Our given point is at: ( 6, 1.5)
+
+// Define a structure representing a point on a cartesian plane
+struct Point {
+    var x : Double = 0.0
+    var y : Double = 0.0
+}
+// Create an instance of the point in our problem
+var cabinSite = Point(x: 6, y: 1.5)
+
+// Define a structure that represents slope
+struct Slope {
+    var rise : Double = 1.0
+    var run : Double = 1.0
+}
+
+// Define a structure that represents a line
+struct Line {
+    var slope : Slope = Slope(rise: 1.0, run: 1.0)
+    var verticalIntercept : Double = 0.0
+}
+
+// Create an instance of the line from our problem
+var slopeOfExistingRoad = Slope(rise: -1, run: 2)
+var existingRoad = Line(slope: slopeOfExistingRoad, verticalIntercept: 9.5)
+
+/// Returns the slope of a line perpendicular to the provided line
+///
+/// - Parameter givenLine: The line that we are starting from
+/// - Returns: The slope of the line that is perpendicular
+func getSlopeOfPerpendicularLine(from givenLine: Line) -> Slope {
+    
+    return Slope(rise: givenLine.slope.run, run: givenLine.slope.rise * -1)
+    
+}
+
+// Get the perpendicular slope (the slope of the new road from the cabin to the existing road)
+let perpendicularSlope = getSlopeOfPerpendicularLine(from: existingRoad)
+
+
+
